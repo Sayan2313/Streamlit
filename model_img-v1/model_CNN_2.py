9e7d620b386a53e195e3d5b695340e9cca4added
Sayan2313 <sayansinha728@gmail.com>
2025-08-25 16:47:11 +0530

    first commit

---
+++ model_img-v1/model_CNN_2.py
@@ -0,0 +1,67 @@
+from torch import nn
+from torch import load,argmax,softmax
+import json
+# Model 2
+def conv_block(in_channels:int,out_channels:int,kernel_size,stride,
+               padding,bias):
+    seq = nn.Sequential(
+        nn.Conv2d(in_channels=in_channels,
+                            out_channels=out_channels,
+                            kernel_size=kernel_size,
+                            stride=stride,
+                            padding = padding,
+                            bias = bias
+        ),
+        nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=out_channels,
+                            out_channels=out_channels,
+                            kernel_size=kernel_size,
+                            stride=stride,
+                            padding = padding,
+                            bias = bias
+        ),
+        nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=(3,3),stride=(1,1))
+        )
+    return seq
+def classifier_block(input: int,output : int):
+    return nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(input,output)
+        )
+class model_CNN_2(nn.Module):
+    def __init__(self,model_name : str,output_classes:int,in_channels : int = 3):
+        super().__init__()
+        self.model_name = model_name
+        self.layer1_1 = conv_block(in_channels,32,kernel_size=(3,3),stride=(2,2),padding=(1,1),bias=True)
+        self.classifier = classifier_block(32 * 14 * 14,output_classes)
+    def forward(self,x):
+        out = self.layer1_1(x)
+        out = self.classifier(out)
+        return out
+    ## Only For Debugging Purpose
+    def debug_forward(self,x):
+        out = self.layer1_1(x)
+        # out = self.classifier(out)
+        return out
+def getModelObject() -> model_CNN_2:
+    """
+        Needs 64x64 pixels images
+    """
+    try:
+        model = model_CNN_2('dummy',90,3)
+        pre_model_dict = load('best_model.pt')
+        model.load_state_dict(pre_model_dict['model_state_dict'],strict=True)
+        model.eval()
+        return model
+    except Exception:
+        print("Model Loading Problem")
+def predToClass(logits) -> str:
+    softmax_logits = softmax(logits,dim=1)
+    pred = argmax(softmax_logits,dim=1)
+    with open('names_of_the_animals.json','r') as file:
+        dict = json.load(file)
+    return dict[str(pred.item())]
+        
---
+++ model_img-v1/model_online.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from model_CNN_2 import getModelObject,predToClass
+from PIL import Image
+from torchvision.transforms import transforms
+model = getModelObject()
+transformation = transforms.Compose([
+    transforms.Resize((64,64)),
+    transforms.ToTensor()
+])
+# Streamlit UI code
+st.title('Welcome to Image Classifier(v1)')
+st.badge(label='Accuracy 70%',color='green')
+uploaded_file = st.file_uploader("Choose an image...", type=["png","jpg"])
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image")
+    image_tensor = transformation(image)
+    logits = model(image_tensor.unsqueeze(0))
+    prediction = predToClass(logits)
+    st.markdown(f"**Prediction:** {prediction}")
